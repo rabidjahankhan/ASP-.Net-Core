7819bd051e8d380fffae07e6fdafee846f6bf3cd
Rabid-Jahan-Khan <rabidjahankhan@gmail.com>
2025-04-30 02:08:54 +0600

    Add Inheritance folder

---
+++ Inheritance/BankAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class BankAccount
+    {
+        public string AccountNumber { get; set; }
+        public string CustomerName { get; set; }
+        public decimal Balance { get;private set; }
+
+        //public double ServiceCharge { get; set; }
+        //public double InterestRate { get; set; }
+
+        public string Deposit(decimal amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount;
+                return "Deposit successful";
+            }
+            else
+            {
+                return "Deposit failed";
+            }
+        }
+        public string Withdraw(decimal amount)
+        {
+            if (amount > 0 && amount <= Balance)
+            {
+                Balance -= amount;
+                return "Withdrawal successful";
+            }
+            else
+            {
+                return "Withdrawal failed";
+            }
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CheakingAccount cheakingAccount = new CheakingAccount();
+            cheakingAccount.ServiceCharge = 0.02;
+            cheakingAccount.AccountNumber = "987654321";
+            cheakingAccount.CustomerName = "Ishrat Jahan Mahi";
+            cheakingAccount.Deposit(2000);
+            cheakingAccount.Withdraw(1000);
+            Console.WriteLine(cheakingAccount.Balance);
+
+
+            SavingAccount savingAccount = new SavingAccount();
+            savingAccount.InterestRate = 0.05;
+            savingAccount.AccountNumber = "123456789";
+            savingAccount.CustomerName = "Rabid Jahan Khan";
+            savingAccount.Deposit(1000);
+            savingAccount.Withdraw(500);
+            Console.WriteLine(savingAccount.Balance);
+
+        }
+    }
+}
