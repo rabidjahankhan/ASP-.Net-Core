7942f48be4c703dcf6cdef3a970ee43a7d7d6771
Rabid-Jahan-Khan <rabidjahankhan@gmail.com>
2025-06-22 01:03:34 +0600

    Practice 1 done

--- OOP_SOLID_Principles/Practice1/Program.cs
+++ OOP_SOLID_Principles/Practice1/Program.cs
@@ -10,7 +10,30 @@ namespace Practice1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> names = new List<string> ();
+            names.Add("Tareq");
+            names.Add("Afsana");
+            names.Add("Imtiaz");
+            names.Add("Pulok");
+            names.Add("Robin");
+            names.Add("Samia");
+            names.Add("Rupok");
+            names.Add("Rabid");
+
+            Console.WriteLine("Enter search text: ");
+            string userInput = Console.ReadLine();
+
+            var matched = names.Where(name => name.IndexOf(userInput, StringComparison.OrdinalIgnoreCase) >= 0).ToList();
+
+            if ( matched.Count > 0)
+            {
+                Console.WriteLine("Matched Names: " + string.Join(", ", matched));
+            }
+            else
+            {
+                Console.WriteLine("No match found.");
+            }
+
         }
     }
 }
